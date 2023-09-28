258574eb7d10683c09a461b011b9efa3a98a718d
Mạnh Đạt <tranhuudats1@gmail.com>
2023-09-28 14:17:14 +0700

    Add colume fistname,lastname (Users)

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -49,6 +49,16 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $favourites;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
     public function __construct()
     {
         $this->advertisements = new ArrayCollection();
@@ -200,6 +210,30 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
                 $favourite->setUsers(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
 
         return $this;
     }
