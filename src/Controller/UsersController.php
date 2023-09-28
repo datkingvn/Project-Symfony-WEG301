956fdc7c770445c253dedec43dcea07b0c2c1518
Mạnh Đạt <tranhuudats1@gmail.com>
2023-09-28 21:34:46 +0700

    Update Profile

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Advertisements;
 use App\Form\AdvertisementsType;
+use App\Form\EditProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -44,6 +45,32 @@ class UsersController extends AbstractController
         }
 
         return $this->render('users/advertisements/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/users/profile/edit", name="users_profile_edit")
+     */
+    public function editProfile(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Profile Update Successfully!');
+            return $this->redirectToRoute('app_users');
+        }
+
+        return $this->render('users/edit-profile.html.twig', [
             'form' => $form->createView(),
         ]);
     }
