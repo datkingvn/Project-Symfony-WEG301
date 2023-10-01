93c278446b313bac281a04b4857b6e50056a4bb0
Mạnh Đạt <tranhuudats1@gmail.com>
2023-10-01 09:38:20 +0700

    Home Page UI

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AdvertisementsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,10 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(AdvertisementsRepository $advertisementsRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'advertisements' => $advertisementsRepository->findBy(['active' => true], ['created_at' => 'desc']),
         ]);
     }
 }
